58d22c6320d414d27214383df8c4f1676928f7eb
pandaaured <118323804+pandaaured@users.noreply.github.com>
2024-09-23 17:14:22 -0400

    adding fetcher to project, adding features to main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,98 @@
 use std::fs;
+use std::collections::HashMap;
+use std::process::Command;
+
 
 fn main() {
-    let base_directory: String = "src/static/".to_string();
-    let subdirectory_1 = "pittsburgh/";
-    let subdirectory_2 = "prt/";
-    let file_chosen = "agency.txt"; // Function is currently fixed. Wish to change this later.
-    let request = base_directory + subdirectory_1 + subdirectory_2 + file_chosen;
-    let string = fs::read_to_string(request)
-        .expect("Should be able to read file.");
-    let header_index = header_end_index(&string);
+    let data_vec = string_to_vec_of_vec_of_vec();
+    // let agency = vec_to_hashmap_ag(&data_vec[0]);
+    // let calendar_dates = vec_to_hashmap_cd(&data_vec[1]);
+    let routes = vec_to_hashmap_rt(&data_vec[4]);
 
-    let header_string = &string[..header_index].to_string();
-    string_to_vector(header_string);
 
+    // let calendar_dates_vec = &data_vec[1];
+    // let calendar_vec = &data_vec[2];
+    // let routes_vec = &data_vec[4];
+    // let shapes_vec = &data_vec[5];
+    // let stop_times_vec = &data_vec[6];
+    // let stops_vec = &data_vec[7];
+    // let trips_vec = &data_vec[9];
+    println!("{:#?}", routes);
+}
+
+fn vec_to_hashmap_ag(vec: &Vec<Vec<String>>) -> HashMap<String, Agency> {
+    let mut data = HashMap::new();
+    for row in vec {
+        let ag_struct: Agency = Agency 
+            {agency_name: row[1].clone(), 
+             agency_url: row[2].clone(), 
+             agency_timezone: row[3].clone(), 
+             agency_lang: row[4].clone(), 
+             agency_phone: row[5].clone(),
+            agency_fare_url: row[6].clone()};
+        data.insert(row[0].clone(), ag_struct); }   
+    data
+}
+
+fn vec_to_hashmap_cd(vec: &Vec<Vec<String>>) -> HashMap<String, CalendarDates> {
+    let mut data = HashMap::new();
+    for row in vec {
+        let cd_struct: CalendarDates = CalendarDates 
+            {date: row[1].clone(),
+             exception_type: row[2].clone()};
+        data.insert(row[0].clone(), cd_struct); }   
+    data
+}
+
+fn vec_to_hashmap_rt(vec: &Vec<Vec<String>>) -> HashMap<String, Routes> {
+    let mut data = HashMap::new();
+    for row in vec {
+        let rt_struct: Routes = Routes 
+            {agency_id: row[1].clone(),
+             route_short_name: row[2].clone(),
+             route_long_name: row[3].clone(),
+             route_desc: row[4].clone(),
+             route_type: row[5].clone(),
+             route_url: row[6].clone(),
+             route_color: row[7].clone(),
+             route_text_color: row[8].clone()};
+        data.insert(row[0].clone(), rt_struct); }   
+    data
+}
+
+fn string_to_vec_of_vec_of_vec() -> Vec<Vec<Vec<String>>> {
+    let mut double_collection: Vec<Vec<Vec<String>>> = Vec::new();
+    
+    let items: [&str; 10] = ["src/static/pittsburgh/prt/agency.txt",
+    "src/static/pittsburgh/prt/calendar_dates.txt",
+    "src/static/pittsburgh/prt/calendar.txt",
+    "src/static/pittsburgh/prt/feed_info.txt",
+    "src/static/pittsburgh/prt/routes.txt",
+    "src/static/pittsburgh/prt/shapes.txt",
+    "src/static/pittsburgh/prt/stop_times.txt",
+    "src/static/pittsburgh/prt/stops.txt",
+    "src/static/pittsburgh/prt/transfers.txt",
+    "src/static/pittsburgh/prt/trips.txt"];
+    
+    for path in items {
+        let string = 
+        fs::read_to_string(path)
+            .expect("Should be able to read file.");
+        let agency_vec = string_to_vec_of_vec(&string);
+        double_collection.push(agency_vec);
+    }
+
+    double_collection
+}
+
+fn string_to_vec_of_vec(str: &String) -> Vec<Vec<String>> {
     let mut collection: Vec<Vec<String>> = Vec::new();
-
-    let vector: Vec<String> = string.lines().map(String::from).collect();
+    let vector: Vec<String> = str.lines().map(String::from).collect();
     for i in vector.iter() {
         let row = string_to_vector(i);
         collection.push(row);
     }
-
-    println!("{:#?}", collection);
-
-}
-
-fn header_end_index(str: &String) -> usize {
-    let index: Option<usize> = str.find("\n");
-    let mut value: usize = 0;
-    if index != None {
-        value = index.unwrap();
-    }
-    value
+    collection
 }
 
 fn string_to_vector(str: &String) -> Vec<String> {
@@ -40,47 +101,46 @@ fn string_to_vector(str: &String) -> Vec<String> {
     terms
 }
 
-// #[derive(Debug)]
-// struct Agency {
-//     agency_id: i32,
-//     agency_name: String,
-//     agency_url: String,
-//     agency_timezone: String,
-//     agency_lang: String,
-//     agency_phone: String,
-//     agency_fare_url: String
-// }
+#[derive(Debug)]
+struct Agency {
+    agency_name: String,
+    agency_url: String,
+    agency_timezone: String,
+    agency_lang: String,
+    agency_phone: String,
+    agency_fare_url: String
+}
 
-// struct CalendarDates {
-//     service_id: String,
-//     date: i32,
-//     exception_type: String
-// }
+#[derive(Debug)]
+struct CalendarDates {
+    date: String,
+    exception_type: String
+}
 
-// struct Calendar {
-//     service_id: String,
-//     monday: bool,
-//     tuesday: bool,
-//     wednesday: bool,
-//     thursday: bool,
-//     friday: bool,
-//     saturday: bool,
-//     sunday: bool,
-//     start_date: i32,
-//     end_date: i32
-// }
+struct Calendar {
+    service_id: String,
+    monday: bool,
+    tuesday: bool,
+    wednesday: bool,
+    thursday: bool,
+    friday: bool,
+    saturday: bool,
+    sunday: bool,
+    start_date: i32,
+    end_date: i32
+}
 
-// struct Routes {
-//     route_id: String,
-//     agency_id: String,
-//     route_short_name: String,
-//     route_long_name: String,
-//     route_desc: String,
-//     route_type: String,
-//     route_url: String,
-//     route_color: String,
-//     route_text_color: String
-// }
+#[derive(Debug)]
+struct Routes {
+    agency_id: String,
+    route_short_name: String,
+    route_long_name: String,
+    route_desc: String,
+    route_type: String,
+    route_url: String,
+    route_color: String,
+    route_text_color: String
+}
 
 // struct Trips {
 //     route_id: String,
