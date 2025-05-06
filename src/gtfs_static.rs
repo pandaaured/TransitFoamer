ae3764e039a97573c35d977955f3abde832f8203
pandaaured <118323804+pandaaured@users.noreply.github.com>
2025-05-06 12:05:08 -0400

    Adding hashmap variants of vector functions

--- src/gtfs_static.rs
+++ src/gtfs_static.rs
@@ -5,99 +5,186 @@
 //! A library for fetching and analyzing GTFS Static data from a transit feed.
 
 pub mod get {
-    use crate::gtfs_static::{Agency, Calendar, CalendarDates, Routes, StopTimes, Stops, Trips};
-    use std::io::Error;
+    use crate::gtfs_static::{StopTimes, Trips};
     use std::collections::{HashMap, HashSet};
 
-    pub fn agency(file_path: String) -> Result<Vec<Agency>, Error> {
-        let mut agency: Vec<Agency> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("agency.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: Agency = result?;
-            agency.push(record);
+    pub mod vecs {
+        use crate::gtfs_static::{
+            Agency, Calendar, CalendarDates, Routes, StopTimes, Stops, Trips,
+        };
+        use std::io::Error;
+
+        pub fn agency(file_path: String) -> Result<Vec<Agency>, Error> {
+            let mut agency: Vec<Agency> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("agency.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: Agency = result?;
+                agency.push(record);
+            }
+
+            Ok(agency)
         }
 
-        Ok(agency)
+        pub fn calendardates(file_path: String) -> Result<Vec<CalendarDates>, Error> {
+            let mut calendardates: Vec<CalendarDates> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("calendar_dates.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: CalendarDates = result?;
+                calendardates.push(record);
+            }
+
+            Ok(calendardates)
+        }
+
+        pub fn calendar(file_path: String) -> Result<Vec<Calendar>, Error> {
+            let mut calendar: Vec<Calendar> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("calendar.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: Calendar = result?;
+                calendar.push(record);
+            }
+
+            Ok(calendar)
+        }
+
+        pub fn routes(file_path: String) -> Result<Vec<Routes>, Error> {
+            let mut routes: Vec<Routes> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("routes.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: Routes = result?;
+                routes.push(record);
+            }
+
+            Ok(routes)
+        }
+
+        pub fn stops(file_path: String) -> Result<Vec<Stops>, Error> {
+            let mut stops: Vec<Stops> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("stops.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: Stops = result?;
+                stops.push(record);
+            }
+
+            Ok(stops)
+        }
+
+        pub fn stoptimes(file_path: String) -> Result<Vec<StopTimes>, Error> {
+            let mut stoptimes: Vec<StopTimes> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("stop_times.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: StopTimes = result?;
+                stoptimes.push(record);
+            }
+
+            Ok(stoptimes)
+        }
+
+        pub fn trips(file_path: String) -> Result<Vec<Trips>, Error> {
+            let mut trips: Vec<Trips> = Vec::new(); // Initializes the mutable data.
+            let mut path: String = file_path.clone(); // Getting the file path and
+            path.push_str("trips.txt");
+            let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
+            for result in rdr.deserialize() {
+                let record: Trips = result?;
+                trips.push(record);
+            }
+
+            Ok(trips)
+        }
     }
 
-    pub fn calendardates(file_path: String) -> Result<Vec<CalendarDates>, Error> {
-        let mut calendardates: Vec<CalendarDates> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("calendar_dates.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: CalendarDates = result?;
-            calendardates.push(record);
+    pub mod hash {
+        use crate::gtfs_static::{
+            Agency, Calendar, CalendarDates, Routes, StopTimes, Stops, Trips,
+        };
+        use std::collections::HashMap;
+
+        pub fn agency(agency: Vec<Agency>) -> HashMap<String, Agency> {
+            let mut map: HashMap<String, Agency> = HashMap::new();
+            for entry in agency {
+                let key: &String = entry.agency_id.as_ref().unwrap();
+                map.insert(key.to_string(), entry);
+            }
+
+            map
         }
 
-        Ok(calendardates)
-    }
+        pub fn calendardates(
+            calendardates: Vec<CalendarDates>,
+        ) -> HashMap<(String, String), CalendarDates> {
+            let mut map: HashMap<(String, String), CalendarDates> = HashMap::new();
+            for entry in calendardates {
+                let key_one: &String = &entry.service_id;
+                let key_two: &String = &entry.date;
+                map.insert((key_one.to_string(), key_two.to_string()), entry);
+            }
 
-    pub fn calendar(file_path: String) -> Result<Vec<Calendar>, Error> {
-        let mut calendar: Vec<Calendar> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("calendar.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: Calendar = result?;
-            calendar.push(record);
+            map
         }
 
-        Ok(calendar)
-    }
+        pub fn calendar(calendar: Vec<Calendar>) -> HashMap<String, Calendar> {
+            let mut map: HashMap<String, Calendar> = HashMap::new();
+            for entry in calendar {
+                let key: &String = &entry.service_id;
+                map.insert(key.to_string(), entry);
+            }
 
-    pub fn routes(file_path: String) -> Result<Vec<Routes>, Error> {
-        let mut routes: Vec<Routes> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("routes.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: Routes = result?;
-            routes.push(record);
+            map
         }
 
-        Ok(routes)
-    }
+        pub fn routes(routes: Vec<Routes>) -> HashMap<String, Routes> {
+            let mut map: HashMap<String, Routes> = HashMap::new();
+            for entry in routes {
+                let key: &String = &entry.route_id;
+                map.insert(key.to_string(), entry);
+            }
 
-    pub fn stops(file_path: String) -> Result<Vec<Stops>, Error> {
-        let mut stops: Vec<Stops> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("stops.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: Stops = result?;
-            stops.push(record);
+            map
         }
 
-        Ok(stops)
-    }
+        pub fn stops(stops: Vec<Stops>) -> HashMap<String, Stops> {
+            let mut map: HashMap<String, Stops> = HashMap::new();
+            for entry in stops {
+                let key: &String = &entry.stop_id;
+                map.insert(key.to_string(), entry);
+            }
 
-    pub fn stoptimes(file_path: String) -> Result<Vec<StopTimes>, Error> {
-        let mut stoptimes: Vec<StopTimes> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("stop_times.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: StopTimes = result?;
-            stoptimes.push(record);
+            map
         }
 
-        Ok(stoptimes)
-    }
+        pub fn stoptimes(stoptimes: Vec<StopTimes>) -> HashMap<(String, String), StopTimes> {
+            let mut map: HashMap<(String, String), StopTimes> = HashMap::new();
+            for entry in stoptimes {
+                let key_one: &String = &entry.trip_id;
+                let key_two: &String = &entry.stop_sequence;
+                map.insert((key_one.to_string(), key_two.to_string()), entry);
+            }
 
-    pub fn trips(file_path: String) -> Result<Vec<Trips>, Error> {
-        let mut trips: Vec<Trips> = Vec::new(); // Initializes the mutable data.
-        let mut path: String = file_path.clone(); // Getting the file path and
-        path.push_str("trips.txt");
-        let mut rdr = csv::ReaderBuilder::new().from_path(path)?;
-        for result in rdr.deserialize() {
-            let record: Trips = result?;
-            trips.push(record);
+            map
         }
 
-        Ok(trips)
+        pub fn trips(trips: Vec<Trips>) -> HashMap<String, Trips> {
+            let mut map: HashMap<String, Trips> = HashMap::new();
+            for entry in trips {
+                let key: &String = &entry.trip_id;
+                map.insert(key.to_string(), entry);
+            }
+
+            map
+        }
     }
 
     // Needs more proper testing.
@@ -114,7 +201,7 @@ pub mod get {
                 trips_per_route.insert(route, vec![trip]);
             }
         }
-    
+
         trips_per_route
     }
 
@@ -127,21 +214,22 @@ pub mod get {
             if stops_per_trip.contains_key(&trip_id.clone()) {
                 let stop_id = item.stop_id.unwrap();
                 let mut vector = stops_per_trip[&trip_id.clone()].clone();
-                    vector.push(stop_id.clone());
-                    stops_per_trip.insert(trip_id.clone(), vector.clone());
+                vector.push(stop_id.clone());
+                stops_per_trip.insert(trip_id.clone(), vector.clone());
             } else {
                 let stop_id = item.stop_id.unwrap();
                 stops_per_trip.insert(trip_id.clone(), vec![stop_id.clone()]);
             }
         }
-        
+
         stops_per_trip
     }
 
     // Needs more proper testing.
-    pub fn routes_per_stop(trips_per_route: HashMap<String, Vec<String>>, 
-                           stops_per_trip: HashMap<String, Vec<String>>) 
-                           -> HashMap<String, HashSet<String>> {           
+    pub fn routes_per_stop(
+        trips_per_route: HashMap<String, Vec<String>>,
+        stops_per_trip: HashMap<String, Vec<String>>,
+    ) -> HashMap<String, HashSet<String>> {
         let mut routes_per_stop: HashMap<String, HashSet<String>> = HashMap::new();
 
         for route in trips_per_route {
@@ -172,57 +260,57 @@ pub mod get {
         #[test]
         fn agency_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let agency = agency(path.to_string());
+            let agency = vecs::agency(path.to_string());
             println!("{:?}", agency);
         }
 
         #[test]
         fn calendar_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let calendar = calendar(path.to_string());
+            let calendar = vecs::calendar(path.to_string());
             println!("{:?}", calendar);
         }
 
         #[test]
         fn calendardates_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let calendardates = calendardates(path.to_string());
+            let calendardates = vecs::calendardates(path.to_string());
             println!("{:?}", calendardates);
         }
 
         #[test]
         fn routes_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let routes = routes(path.to_string());
+            let routes = vecs::routes(path.to_string());
             println!("{:?}", routes);
         }
 
         #[test]
         fn stops_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let stops = stops(path.to_string());
+            let stops = vecs::stops(path.to_string());
             println!("{:?}", stops);
         }
 
         #[test]
         fn stoptimes_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let stoptimes = stoptimes(path.to_string());
+            let stoptimes = vecs::stoptimes(path.to_string());
             println!("{:?}", stoptimes);
         }
 
         #[test]
         fn trips_prints() {
             let path = "src/static/pittsburgh/prt/";
-            let trips = trips(path.to_string());
+            let trips = vecs::trips(path.to_string());
             println!("{:?}", trips);
         }
 
         #[test]
         fn routes_per_stop_test() {
             let path = "src/static/pittsburgh/prt/";
-            let tpr = trips_per_route(trips(path.to_string()).unwrap());
-            let spt = stops_per_trip(stoptimes(path.to_string()).unwrap());
+            let tpr = trips_per_route(vecs::trips(path.to_string()).unwrap());
+            let spt = stops_per_trip(vecs::stoptimes(path.to_string()).unwrap());
             let rps = routes_per_stop(tpr, spt);
             println!("{:?}", rps);
         }
