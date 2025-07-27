6f6965d49543bbcc1432b496a691049f672ae3a4
hauschkaured <118323804+hauschkaured@users.noreply.github.com>
2025-07-27 01:29:17 -0400

    possible compile flags for testing + updated tests (wip)

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// For basic function testing by passing function flags as compilation
+// flags.
+//
+use std::env;
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    
+
+
+}
+
