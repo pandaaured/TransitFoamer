67f840088279cfa07090f61a73e2424c098d2d4f
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-05-23 16:45:11 -0400

    Update vite configuration

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/schedule': "http://127.0.0.1:8080",
+      '/routes': "http://127.0.0.1:8080",
+      '/rtlist': "http://127.0.0.1:8080",
+      '/stoptimes': "http://127.0.0.1:8080",
+    }
+
   }
 })
