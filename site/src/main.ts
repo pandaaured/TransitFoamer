d62de1e909e7140148fdd53367c9e150c8d2e9eb
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-05-24 15:08:38 -0400

    main just calls the Svelte app

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,29 +1,11 @@
-import './styles.css'
-import { getSchedule, getRoutes } from './api'
-import { renderSchedule } from './render'
-import { renderRouteList } from './routes-page'
+import './styles/base.css'
+import './styles/nav.css'
+import './styles/home.css'
+import './styles/route-list.css'
+import './styles/schedule.css'
+import './styles/sequence.css'
+import './styles/header.css'
+import { mount } from 'svelte'
+import App from './App.svelte'
 
-const container = document.getElementById('app')!
-
-async function navigate() {
-  const hash = window.location.hash
-
-  if (hash.startsWith('#/schedule/')) {
-    const routeId = hash.replace('#/schedule/', '')
-    container.innerHTML = '<p>Loading schedule...</p>'
-    const schedule = await getSchedule(routeId)
-    if (schedule) {
-      renderSchedule(schedule, container)
-    } else {
-      container.innerHTML = '<p>Failed to load schedule.</p>'
-    }
-  } else {
-    // Default: route list
-    container.innerHTML = '<p>Loading routes...</p>'
-    const routes = await getRoutes()
-    renderRouteList(routes, container)
-  }
-}
-
-window.addEventListener('hashchange', navigate)
-navigate()
+mount(App, { target: document.getElementById('app')! })
